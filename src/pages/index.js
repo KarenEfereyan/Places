eeaab2d53fe1f63faa2fab12c4f4abdd5e4c70b1
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-11-30 11:42:20 +0100

    Trips section complete
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <Layout>
           <Header  />
           <Hero />
-          <Trips />
+          <Trips heading = "Our Favorite Destinations" />
       </Layout>
   
   )
