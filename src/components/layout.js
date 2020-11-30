03e73edb4451ba9a6a47e891f1770ee76f51307d
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-11-30 14:29:45 +0100

    Footer starting files
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import GlobalStyles from './styles/GlobalStyles'
+import GlobalStyles from './styles/GlobalStyles';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
     return (
         <React.Fragment>
         <GlobalStyles />
         <main>{children}</main>
+        <Footer />
         </React.Fragment>
     )
 }
