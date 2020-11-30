99adcbd316a2c129ed88e7dff8b35ae5cd364a91
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-11-30 18:40:30 +0100

    Footer section done
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,11 +28,12 @@ const Footer = () => {
                 
                 <FooterLinkItems>
                     <FooterLinkTitle> Social Media</FooterLinkTitle>
-                    <FooterLink to ="/">Instagra,</FooterLink>
+                    <FooterLink to ="/">Instagram</FooterLink>
                     <FooterLink to = "/" >Facebook</FooterLink>
                     <FooterLink to = "/">Youtube</FooterLink>
                     <FooterLink to = "/">Twitter</FooterLink>
                 </FooterLinkItems>   
+                
             </FooterLinksWrapper>
         </FooterContainer>
     )
@@ -41,19 +42,26 @@ const Footer = () => {
 export default Footer;
 
 const FooterContainer = styled.div`
-padding: 5rem calc(100vw - 1300px ) /2);
-display:grid;
-grid-template-columns:repeat(2, 1fr);
-color:#000;
-background:#fafafb;
+    padding: 5rem calc((100vw - 1300px ) /2);
+    width : 100%;
+    color:#000;
+    background:#fafafb;
 `
 
 const FooterDesc = styled.div`
-padding: 0 2rem;
+    padding: 0 2rem;
 
 h1{
-    margin-bottom:3rem;
+    margin-bottom:2rem;
     color:#f26a2e;
+}
+p{
+    font-size: clamp(0.85rem, 2vw, 1.2rem);
+    line-height:1.5;
+    color:#c9c9c9;
+    ${'' /* color:#000;
+    opacity:.75; */}
+    
 }
 
 @media screen and (max-width:400px){
@@ -62,19 +70,23 @@ h1{
 `
 
 const FooterLinksWrapper = styled.div`
-display:grid;
-grid-template-columns: repeat(2, 1fr);
+    display:grid;
+    grid-template-columns: repeat(4, 1fr);
 
 @media screen and (max-width: 820px){
+    grid-template-columns:1fr 1fr;
+}
+
+@media screen and (max-width: 400px){
     grid-template-columns:1fr;
 }
 `
 
 const FooterLinkItems = styled.div`
-display:flex;
-flex-direction: column;
-align-items:flex-start;
-padding: 1rem 2rem;
+    display:flex;
+    flex-direction: column;
+    align-items:flex-start;
+    padding: 1rem 2rem;
 
 @media screen and (max-width: 400px){
     padding:1rem;
@@ -82,18 +94,19 @@ padding: 1rem 2rem;
 `
 
 const FooterLinkTitle = styled.h2`
-font-size:14px;
-margin-bottom: 16px;
+    font-size:clamp(1rem, 2.1vw, 1.2rem);
+    margin-bottom: 1.25rem;
+    opacity:.75;
 `
 
 const FooterLink = styled(Link)`
-text-decoration:none;
-margin-bottom: 0.5rem;
-font-size:14px;
-color:#3d3d4e;
+    text-decoration:none;
+    margin-bottom: 0.75rem;
+    font-size:clamp(0.75rem, 1.8vw, 1rem);
+    color:#c9c9c9;
 
 &:hover{
     color:#f26a2e;
     transition: 0.3s ease-out;
-    }
+}
 `
